fc4513a105b679bec3c364af25d7fea5b66cbeff
Lorena Araújo <lorena-naraujo@users.noreply.github.com>
2026-07-19 18:48:21 -0300

    chore(db):adiciona scripts SQL de migrations para o backend

---
+++ database/migrations/V1__tabela_base.sql
@@ -0,0 +1,15 @@
+--1.Criar tabela independente de FK
+
+--cria a tabela secretarias
+CREATE TABLE IF NOT EXISTS secretarias(
+	id_secretaria UUID NOT NULL,
+	nome_secretaria VARCHAR(150) NOT NULL,
+	sigla VARCHAR(10) NOT NULL UNIQUE,
+	endereco VARCHAR(255),
+	cep VARCHAR(8) NOT NULL,
+	creadoEm TIMESTAMPTZ NOT NULL,
+	atualizadoEm TIMESTAMPTZ NOT NULL,
+
+    --cria a chave primária para a tabela secretarias
+	CONSTRAINT pk_secretarias PRIMARY KEY(id_secretaria)
+);
