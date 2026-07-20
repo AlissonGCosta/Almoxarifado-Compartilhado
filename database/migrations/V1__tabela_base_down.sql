3e1af5eb4ba060fccf6ee7595bce37de19bf550f
Lorena Araújo <lorena-naraujo@users.noreply.github.com>
2026-07-19 23:23:18 -0300

    feat(db):adiciona os arquivos com os  scripts de exclusão de tabelas SQL(complementares aos scripts de criação de tabelas SQL) e renomeia arquivos existentes para melhor padronização

---
+++ database/migrations/V1__tabela_base_down.sql
@@ -0,0 +1,4 @@
+-- Criação de comandos de exclusão das tabela principal
+
+-- Exclui as tabelas secretarias
+DROP TABLE IF EXISTS secretarias;
---
+++ database/migrations/V2__tabelas_relacionamentos_down.sql
@@ -0,0 +1,19 @@
+--Criação de comandos de exclusão das tabelas relacionadas
+
+-- cria comando de exclusão da tabela itens_pedido_compra
+DROP TABLE IF EXISTS itens_pedido_compra;
+
+-- cria comando de exclusão da tabela pedidos_compra
+DROP TABLE IF EXISTS pedidos_compra;
+
+-- cria comando de exclusão da tabela itens_pedido_transferencia
+DROP TABLE IF EXISTS itens_pedido_transferencia;
+
+-- cria comando de exclusão da tabela pedidos_transferencia
+DROP TABLE IF EXISTS pedidos_transferencia;
+
+-- cria comando de exclusão da tabela produtos
+DROP TABLE IF EXISTS produtos;
+
+-- cria comando de exclusão da tabela usuarios
+DROP TABLE IF EXISTS usuarios;
